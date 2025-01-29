dc90de537effcbe545b8b0b95c899f1f0db29aac
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-01-29 11:35:58 -0300

    creating a balance variable and methods

--- src/chap03/Account.java
+++ src/chap03/Account.java
@@ -1,15 +1,30 @@
 package chap03;
 
-// Figura 3.1: Account.java
-// Classe Account que contém uma variável de instância name
-// e metodos para configurar e obter seu valor
-
 public class Account {
     private String name;    // variável de instância
+    private double balance; // variável de instância
 
-    // o construtor inicializa name com nome do parâmetro
-    public Account(String name) {   // o nome do construtor é nome da classe
-        this.name = name;
+    // Construtor de Account que recebe dois parâmetros
+    public Account(String name, double balance) {   // o nome do construtor é nome da classe
+        this.name = name;   // atribui name à variável de instância name
+
+        // valida que o balance é maior que 0.0; se não for,
+        // a variável de instância balance mantém seu valor inicial padrão de 0.0
+        if (balance > 0.0) {    // se o saldo for válido
+            this.balance = balance; // o atribui a variável de instância balance
+        }
+    }
+
+    // metodo que deposita (adiciona) apenas uma quantia válida no saldo
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0.0) {      // se depositAmount for válido
+            balance = balance + depositAmount;  // o adiciona ao saldo
+        }
+    }
+
+    // metodo retorna o saldo da conta
+    public double getBalance() {
+        return balance;
     }
 
     // metodo para definir o nome no objeto
