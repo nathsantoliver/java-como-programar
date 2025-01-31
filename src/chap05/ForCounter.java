0d9320b389279ef7af068bd53bde4617d4b675ce
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-01-31 09:16:12 -0300

    chap 5 - controlled repetition using for. fg5.2

---
+++ src/chap05/ForCounter.java
@@ -0,0 +1,13 @@
+package chap05;
+
+public class ForCounter {
+    public static void main(String[] args) {
+        // o cabeçalho da instrução for inclui inicialização,
+        // condição de continuação do loop e incremento
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.printf("%d ", counter);
+        }
+
+        System.out.println();
+    }
+}
