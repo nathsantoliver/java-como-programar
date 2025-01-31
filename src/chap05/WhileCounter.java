a217abdc8159dabce3489aad6b8d5a5bc2180aed
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-01-31 09:02:25 -0300

    chap 5 - controlled repetition using while. fg5.1

---
+++ src/chap05/WhileCounter.java
@@ -0,0 +1,14 @@
+package chap05;
+
+public class WhileCounter {
+    public static void main(String[] args) {
+        int counter = 1;  // declara e inicializa a variável de controle
+
+        while (counter <= 10) {
+            System.out.printf("%d ", counter);
+            ++counter;  // variável de controle de incremento
+        }
+
+        System.out.println();
+    }
+}
