3087995cb32eadbe9bc90d262dceb554ad541cea
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-04 08:58:41 -0300

    chap 7 - initializing an array. fg7.2

---
+++ src/chap07/InitArray.java
@@ -0,0 +1,15 @@
+package chap07;
+
+public class InitArray {
+    public static void main(String[] args) {
+        // declara array variável e o inicializa com um objeto
+        int[] array = new int[10];  // cria o objeto array
+
+        System.out.printf("%s%8s%n", "Index", "Value"); // títulos de coluna
+
+        // gera saída do valor de cada elemento do array
+        for (int counter = 0; counter < array.length; counter++) {
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+        }
+    }
+}
