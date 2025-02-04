4fec5b73a525756ca0a47dddb0cdfff46b5d5a1f
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-04 09:10:51 -0300

    chap 7 - calculating values to be placed on the array elements. fg7.4

---
+++ src/chap07/InitArray3.java
@@ -0,0 +1,20 @@
+package chap07;
+
+public class InitArray3 {
+    public static void main(String[] args) {
+        final int ARRAY_LENGTH = 10;    // declara constante
+        int[] array = new int[ARRAY_LENGTH];    // cria o array
+
+        // gera saída do valor de cada elemento do array
+        for (int counter = 0; counter < array.length; counter++) {
+            array[counter] = 2 + 2 * counter;
+        }
+
+        System.out.printf("%s%8s%n", "Index", "Value"); // títulos de coluna
+
+        // gera saída do valor de cada elemento do array
+        for (int counter = 0; counter < array.length; counter++) {
+            System.out.printf("%5d%8d%n", counter, array[counter]);
+        }
+    }
+}
