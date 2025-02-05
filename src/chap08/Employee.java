4d83b6fff515a1b3de4276cec4a310832f87aab7
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-05 14:24:17 -0300

    chap 8 - compound example. fg8.7, 8.8 and 8.9

---
+++ src/chap08/Employee.java
@@ -0,0 +1,25 @@
+package chap08;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private Date hireDate;
+
+    // construtor para inicializar nome, data de nascimento e data de contratação
+
+    public Employee(String firstName, String lastName, Date birthDate, Date hireDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.hireDate = hireDate;
+    }
+
+    // converte Employee em formato de String
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s  Hired: %s  Birthday: %s",
+                lastName, firstName, hireDate, birthDate);
+    }
+}
