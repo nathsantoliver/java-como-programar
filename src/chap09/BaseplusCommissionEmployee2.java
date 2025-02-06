5898ae5731fc926657e3a36ac2a4d19e8eb47883
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-06 10:14:10 -0300

    chap 9 - ERROR: private members of superclass cannot to be accessed by the subclass. fg.9.8

---
+++ src/chap09/BaseplusCommissionEmployee2.java
@@ -0,0 +1,53 @@
+package chap09;
+
+public class BaseplusCommissionEmployee2 extends CommissionEmployee {
+    private double baseSalary;
+
+    // construtor de seis argumentos
+    public BasePlusCommissionEmployee2(String firstName, String lastName,
+                                      String socialSecurityNumber, double grossSales,
+                                      double commissionRate, double baseSalary) {
+        // chamada explícita para o construtor CommissionEmployee da superclasse
+        super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
+
+        // se baseSalary é inválido, lança uma exceção
+        if (baseSalary < 0.0) {
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+        }
+
+        this.baseSalary = baseSalary;
+    }
+
+    // configura o salário-base
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary < 0.0) {
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+        }
+
+        this.baseSalary = baseSalary;
+    }
+
+    // retorna o salário-base
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    // calcula os lucros
+    @Override
+    public double earnings() {
+        // não permitido: commissionRate e grossSales privado em superclasse
+        return baseSalary + (commissionRate * grossSales);
+    }
+
+    // retorna a representação de String de BasePlusCommissionEmployee
+    @Override   // indica que esse metodo substitui um metodo da superclasse
+    public String toString() {
+        // não permitido: tenta acessar membros private da superclasse
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2f",
+                "commission employee", firstName, lastName,
+                "social security number", socialSecurityNumber,
+                "gross sales", grossSales,
+                "commission rate", commissionRate,
+                "base salary", baseSalary);
+    }
+}
