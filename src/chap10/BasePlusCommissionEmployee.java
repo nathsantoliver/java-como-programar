bd46ee4535797ba64524862496f37c88f89fda1a
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-09 16:33:58 -0300

    chap 10 - referencing superclass and subclass. fg.10.1

--- src/chap10/BasePlusCommissionEmployee.java
+++ src/chap10/BasePlusCommissionEmployee.java
@@ -1,17 +1,14 @@
 package chap10;
 
-public class BasePlusCommissionEmployee {
-    private final String firstName;
-    private final String lastName;
-    private final String socialSecurityNumber;
-    private double grossSales;  // vendas brutas mensais
-    private double commissionRate;  // porcentagem da comissão
+public class BasePlusCommissionEmployee extends CommissionEmployee {
+
     private double baseSalary;  // salário-base por semana
 
     // construtor de seis argumentos
     public BasePlusCommissionEmployee(String firstName, String lastName,
                                       String socialSecurityNumber, double grossSales,
                                       double commissionRate, double baseSalary) {
+        super(firstName, lastName, socialSecurityNumber, grossSales, commissionRate);
         // a chamada implícita para construtor padrão de Object ocorre aqui
         // se grossSales é inválido, lança uma exceção
         if (grossSales < 0.0) {
@@ -28,63 +25,6 @@ public class BasePlusCommissionEmployee {
             throw new IllegalArgumentException("Base salary must be >= 0.0");
         }
 
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.socialSecurityNumber = socialSecurityNumber;
-        this.grossSales = grossSales;
-        this.commissionRate = commissionRate;
-        this.baseSalary = baseSalary;
-    }
-
-    // retorna o nome
-    public String getFirstName() {
-        return firstName;
-    }
-
-    // retorna o sobrenome
-    public String getLastName() {
-        return lastName;
-    }
-
-    // retorna o número do seguro social
-    public String getSocialSecurityNumber() {
-        return socialSecurityNumber;
-    }
-
-    // configura a quantidade de vendas brutas mensais
-    public void setGrossSales(double grossSales) {
-        if (grossSales < 0.0) {
-            throw new IllegalArgumentException("Gross sales must be >= 0.0");
-        }
-
-        this.grossSales = grossSales;
-    }
-
-    // retorna a quantidade de vendas brutas
-    public double getGrossSales() {
-        return grossSales;
-    }
-
-    // configura a taxa de comissão
-    public void setCommissionRate(double commissionRate) {
-        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
-            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
-        }
-
-        this.commissionRate = commissionRate;
-    }
-
-    // retorna a taxa de comissão
-    public double getCommissionRate() {
-        return commissionRate;
-    }
-
-    // configura o salário-base
-    public void setBaseSalary(double baseSalary) {
-        if (baseSalary < 0.0) {
-            throw new IllegalArgumentException("Base salary must be >= 0.0");
-        }
-
         this.baseSalary = baseSalary;
     }
 
@@ -94,18 +34,16 @@ public class BasePlusCommissionEmployee {
     }
 
     // calcula os lucros
+    @Override
     public double earnings() {
-        return baseSalary + (commissionRate * grossSales);
+        return getBaseSalary() + super.earnings();
     }
 
     // retorna a representação de String de BasePlusCommissionEmployee
     @Override   // indica que esse metodo substitui um metodo da superclasse
     public String toString() {
-        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2f",
-                "commission employee", firstName, lastName,
-                "social security number", socialSecurityNumber,
-                "gross sales", grossSales,
-                "commission rate", commissionRate,
-                "base salary", baseSalary);
+        // não permitido: tenta acessar membros private da superclasse
+        return String.format("%s %s%n%s: %.2f", "base salaried",
+                super.toString(), "base salary", getBaseSalary());
     }
 }
