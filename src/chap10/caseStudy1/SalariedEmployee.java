423177b3cc8da908e0a076701d1c8e98f619bb38
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-09 21:04:56 -0300

    chap 10 - case study payment file system using polymorphism. fg.10.4 - 10.9

---
+++ src/chap10/caseStudy1/SalariedEmployee.java
@@ -0,0 +1,45 @@
+package chap10.caseStudy1;
+
+public class SalariedEmployee extends Employee {
+    private double weeklySalary;
+
+    // construtor
+    public SalariedEmployee(String firstName, String lastName,
+                            String socialSecurityNumber, double weeklySalary) {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (weeklySalary < 0.0) {
+            throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+        }
+
+        this.weeklySalary = weeklySalary;
+    }
+
+    // configura o salário
+    public void setWeeklySalary(double weeklySalary) {
+        if (weeklySalary < 0.0) {
+            throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+        }
+
+        this.weeklySalary = weeklySalary;
+    }
+
+    // retorna o salário
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    // calcula os rendimentos; sobrescreve o metodo earnings em Employee
+    @Override
+    public double earnings() {
+        return getWeeklySalary();
+    }
+
+    // retorna a representação String do objeto SalariedEmployee
+
+    @Override
+    public String toString() {
+        return String.format("Salaried employee: %s%n%s: $%,.2f",
+                super.toString(), "weekly salary", getWeeklySalary());
+    }
+}
