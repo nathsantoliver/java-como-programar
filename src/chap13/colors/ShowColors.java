9a9893b72623ec500aa97497646d248850613471
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-13 10:59:56 -0300

    chap 13 - Using colors. fg.13.5 - 13.6

---
+++ src/chap13/colors/ShowColors.java
@@ -0,0 +1,16 @@
+package chap13.colors;
+
+import javax.swing.JFrame;
+
+public class ShowColors {
+    // executa o aplicativo
+    public static void main(String[] args) {
+        // cria o frame para ColorJPanel
+        JFrame frame = new JFrame("Using colors");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ColorJPanel colorJPanel = new ColorJPanel();
+        frame.add(colorJPanel);
+        frame.setSize(400, 180);
+        frame.setVisible(true);
+    }
+}
