2ac4ba8c26ff9e2d0855abbef4fabdcdb6dfe5fc
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-14 14:42:01 -0300

    chap 14 - validating infos with RegEx. fg.14.20 -- 14.21

---
+++ src/chap14/regEx/ValidateInput.java
@@ -0,0 +1,40 @@
+package chap14.regEx;
+
+public class ValidateInput {
+
+    // valida nome
+    public static boolean validateFirstName(String firstName) {
+        return firstName.matches("[A-Z][a-zA-Z]*");
+    }
+
+    // valida sobrenome
+    public static boolean validateLastName(String lastName) {
+        return lastName.matches("[a-zA-Z]+(['-][a-zA-Z]+)*");
+    }
+
+    // valida endereço
+    public static boolean validateAddress(String address) {
+        return address.matches("\\d+\\s+([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
+
+    // valida cidade
+    public static boolean validateCity(String city) {
+        return city.matches("([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
+
+    // valida estado
+    public static boolean validateState(String state) {
+        return state.matches("([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
+
+    // valida CEP
+    public static boolean validateZip(String zip) {
+        return zip.matches("\\d{5}");
+    }
+
+    // valida telefone
+    public static boolean validatePhone(String phone) {
+        return phone.matches("[1-9]\\d{2}-[1-9]\\d{2}-\\d{4}");
+    }
+
+}
