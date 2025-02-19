88bb963bb83207fe9f2cad5bd753bd0c4a13e148
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-19 11:25:43 -0300

    chap 17 - example of lambdas and streams with an string array. fg.17.7

---
+++ src/chap17/ArraysAndStreams2.java
@@ -0,0 +1,35 @@
+package chap17;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ArraysAndStreams2 {
+    public static void main(String[] args) {
+
+        String[] strings = { "Red", "orange", "Yellow", "green", "Blue", "indigo", "Violet" };
+
+        // exibe strings originais
+        System.out.printf("Original strings: %s%n", Arrays.asList(strings));
+
+        // strings em maiúsculas
+        System.out.printf("strings in uppercase: %s%n",
+                Arrays.stream(strings)
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toList()));
+
+        // strings menores que "n" (sem distinção maiúsc/minúsc) em ordem crescente
+        System.out.printf("strings greater than n sorted ascending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER)
+                        .collect(Collectors.toList()));
+
+        // strings menores que "n" (sem distinção maiúsc/minúsc) em ordem decrescente
+        System.out.printf("strings greater than n sorted descending: %s%n",
+                Arrays.stream(strings)
+                        .filter(s -> s.compareToIgnoreCase("n") < 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                        .collect(Collectors.toList()));
+
+    }
+}
