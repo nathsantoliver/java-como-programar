b1a6c2f6c1359234326d970ae41146829de68fac
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-19 20:17:23 -0300

    chap 17 - rolling a die six thousand times with streams. fg.17.19

---
+++ src/chap17/RandomIntStream.java
@@ -0,0 +1,21 @@
+package chap17;
+
+import java.security.SecureRandom;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class RandomIntStream {
+    public static void main(String[] args) {
+
+        SecureRandom random = new SecureRandom();
+
+        // rolando um dado 6.000.000 vezes e resumindo os resultados
+        System.out.printf("%-6s%s%n", "Face", "Frequency");
+        random.ints(6_000_000,1, 7)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()))
+                .forEach((face, frequency) ->
+                        System.out.printf("%-6s%s%n", face, frequency));
+    }
+}
