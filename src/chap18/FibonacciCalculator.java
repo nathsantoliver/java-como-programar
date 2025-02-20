8c0e240d526dd5f52cc295e1a0144cb89dd25ada
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-20 09:30:36 -0300

    chap 18 - fibonacci recursive method. fg.18.5

---
+++ src/chap18/FibonacciCalculator.java
@@ -0,0 +1,27 @@
+package chap18;
+
+import java.math.BigInteger;
+
+public class FibonacciCalculator {
+
+    private static BigInteger TWO = BigInteger.valueOf(2);
+
+    // declaração recursiva do metodo fibonacci
+    public static BigInteger fibonacci(BigInteger number) {
+        if (number.equals(BigInteger.ZERO) || number.equals(BigInteger.ONE)) {  // casos básicos
+            return number;
+        } else {    // passo de recursão
+            return fibonacci(number.subtract(BigInteger.ONE))
+                    .add(fibonacci(number.subtract(TWO)));
+        }
+    }
+
+    // exibe os valores de Fibonacci de 0 a 40
+    public static void main(String[] args) {
+        for (int counter = 0; counter <= 40; counter++) {
+            System.out.printf("Fibonacci of %d is: %d%n",
+                    counter, fibonacci(BigInteger.valueOf(counter)));
+        }
+    }
+
+}
