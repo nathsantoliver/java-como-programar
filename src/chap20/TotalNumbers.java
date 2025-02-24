e4bc4fa8a2dca8a21fac45a3310eccf73d40e6c5
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-02-24 11:18:22 -0300

    chap 20 - wildcards in methods that accept type parameters. fg.20.13

---
+++ src/chap20/TotalNumbers.java
@@ -0,0 +1,31 @@
+package chap20;
+
+import java.util.ArrayList;
+
+public class TotalNumbers {
+    public static void main(String[] args) {
+        // cria, inicializa e gera saída de ArrayList de números contendo
+        // Integers e Doubles, e então exibe o total dos elementos
+        Number[] numbers = { 1, 2.4, 3, 4.1 };  //Integers e Doubles
+        ArrayList<Number> numberList = new ArrayList<>();
+
+        for (Number element : numbers) {
+            numberList.add(element);    // insere cada número na numberList
+        }
+
+        System.out.printf("numberList contains: %s%n", numberList);
+        System.out.printf("Total of the elements in numberList: %.1f%n", sum(numberList));
+    }
+
+    // calcula o total de elementos em ArrayList
+    public static double sum(ArrayList<Number> list) {
+        double total = 0;   // inicializa o total
+
+        // calcula a soma
+        for (Number element : list) {
+            total += element.doubleValue();
+        }
+
+        return total;
+    }
+}
