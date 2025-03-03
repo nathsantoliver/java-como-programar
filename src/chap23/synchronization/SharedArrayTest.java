590ac1217b0815b369f276a09b381a67d6829bef
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-03-03 10:01:06 -0300

    chap 23 - sharing of unsynchronized mutable data. fg.23.5 -- 23.7

---
+++ src/chap23/synchronization/SharedArrayTest.java
@@ -0,0 +1,39 @@
+package chap23.synchronization;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SharedArrayTest {
+    public static void main(String[] args) {
+
+        // constrói o objeto compartilhado
+        SimpleArray sharedSimpleArray = new SimpleArray(6);
+
+        // cria duas tarefas a gravar no SimpleArray compartilhado
+        ArrayWriter writer1 = new ArrayWriter(1, sharedSimpleArray);
+        ArrayWriter writer2 = new ArrayWriter(11, sharedSimpleArray);
+
+        // executa as tarefas com um ExecutorService
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(writer1);
+        executorService.execute(writer2);
+
+        executorService.shutdown();
+
+        try {
+            // espera 1 minuto para que ambos os escritores terminem a execução
+            boolean tasksEnded = executorService.awaitTermination(1, TimeUnit.MINUTES);
+
+            if (tasksEnded) {
+                System.out.printf("%nContents of SimpleArray:%n");
+                System.out.println(sharedSimpleArray);  // imprime o conteúdo
+            } else {
+                System.out.println("Timed out while waiting for tasks to finish.");
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
---
+++ src/chap23/synchronization/SimpleArray.java
@@ -0,0 +1,42 @@
+package chap23.synchronization;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class SimpleArray {  // ATENÇÃO: NÃO SEGURO PARA THREADS!
+
+    private static final SecureRandom generator = new SecureRandom();
+    private final int[] array;  // array de inteiros compartilhado
+    private int writeIndex = 0; // índice compartilhado do próximo elemento a gravar
+
+    // cria um SimpleArray de um determinado tamanho
+    public SimpleArray(int size) {
+        array = new int[size];
+    }
+
+    // adiciona um valor ao array compartilhado
+    public void add(int value) {
+        int position = writeIndex; // armazena o índice de gravação
+
+        try {
+            // coloca a thread para dormir durante 0 a 499 milissegundos
+            Thread.sleep(generator.nextInt(500));
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt(); // reinterrompe a thread
+        }
+
+        // coloca o valor no elemento apropriado
+        array[position] = value;
+        System.out.printf("%s wrote %2d to element %d.%n",
+                Thread.currentThread().getName(), value, position);
+
+        ++writeIndex;   // índice de incremento de elemento a ser gravado depois
+        System.out.printf("Next write index: %d%n", writeIndex);
+    }
+
+    // usado para gerar saída do conteúdo do array de inteiros compartilhado
+    public String toString() {
+        return Arrays.toString(array);
+    }
+
+}
